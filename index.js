b292efbe6837762b1b8d0c2b7e77a61bc1d0514d
Azher Farooqi <azher@momentum.io>
2024-12-25 23:14:03 -0800

    chore: Add basic express setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require("express");
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.render("Hello world!");
+app.use(cors('*'));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api-vi', routes);
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: '404 Not Found',
+    message: 'Not Found',
+  });
 });
 
 app.listen(port, () => {
-  console.log("Listening on port " + port);
-})
+  console.log(`Server started on port ${port}`);
+});
