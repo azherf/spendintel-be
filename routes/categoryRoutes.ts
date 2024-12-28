1b6a4833e8b3c2fd6077926ed3da1a24084772ac
Azher Farooqi <azher@momentum.io>
2024-12-27 19:14:05 -0800

    feat: Create GET APIs for all categories and a single category

--- routes/categoryRoutes.ts
+++ routes/categoryRoutes.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 import authMiddleware from "../middleware/authMiddleware";
-import { createCategory, deleteCategory, getCategories, updateCategory } from "../controllers/categoryController";
+import { createCategory, deleteCategory, getCategories, getCategory, updateCategory } from "../controllers/categoryController";
 import { AuthenticatedRequest } from "../types/express";
 
 const router: Router = express.Router();
 
 router.get("/", authMiddleware, (req, res) => getCategories(req as AuthenticatedRequest, res));
+router.get("/:id", authMiddleware, (req, res) => getCategory(req as AuthenticatedRequest, res));
 router.post("/", authMiddleware, (req, res) => createCategory(req as AuthenticatedRequest, res));
 router.put("/", authMiddleware, (req, res) => updateCategory(req as AuthenticatedRequest, res));
 router.delete("/", authMiddleware, (req, res) => deleteCategory(req as AuthenticatedRequest, res));
