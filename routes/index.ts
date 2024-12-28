30721d6b5e8516cac02f696903fbbf0e5501c72b
Azher Farooqi <azher@momentum.io>
2024-12-27 22:30:54 -0800

    feat: Add skeleton for mode of payment with routes and controller

--- routes/index.ts
+++ routes/index.ts
@@ -2,14 +2,16 @@ import express, { Router } from "express";
 import authRoutes from "./authRoutes";
 import categoryRoutes from "./categoryRoutes";
 import currencyRoutes from "./currencyRoutes";
+import modeOfPaymentRoutes from "./modeOfPaymentRoutes";
 import userRoutes from "./userRoutes";
 
 const router: Router = express.Router();
 
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/currencies", currencyRoutes);
+router.use("/modes-of-payment", modeOfPaymentRoutes);
+router.use("/user", userRoutes);
 
 
 export default router;
